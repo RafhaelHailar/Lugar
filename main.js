bb2f99b72a78da2e03246861b377e4ac75299708
Rafhael <rafhael4hailar@gmail.com>
2023-12-01 23:29:45 +0800

    Indicators animation >

--- main.js
+++ main.js
@@ -1,40 +1,115 @@
-let current = 0;
-const total = 4;
+const data = [
+    {
+        name: "AKLAN",
+        description: ` Aklan, officially the Province of Aklan, is a province in the Western Visayas region of the Philippines. Its capital is Kalibo. The province is situated in the northwest portion of Panay Island, bordering Antique to the southwest, and Capiz to the southeast.`,
+        backdrop: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Boracay_White_Beach.png",
+        feature: "Boracay",
+        media: "https://lp-cms-production.imgix.net/2023-05/shutterstock_editorial_1741828760.jpg?auto=format&fit=crop&ar=1:1&q=75&w=1200"
+    },
+    {
+        name: "AKLAN",
+        description: ` Aklan, officially the Province of Aklan, is a province in the Western Visayas region of the Philippines. Its capital is Kalibo. The province is situated in the northwest portion of Panay Island, bordering Antique to the southwest, and Capiz to the southeast.`,
+        backdrop: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Boracay_White_Beach.png",
+        feature: "Boracay",
+        media: "https://lp-cms-production.imgix.net/2023-05/shutterstock_editorial_1741828760.jpg?auto=format&fit=crop&ar=1:1&q=75&w=1200"
+    },
+    {
+        name: "AKLAN",
+        description: ` Aklan, officially the Province of Aklan, is a province in the Western Visayas region of the Philippines. Its capital is Kalibo. The province is situated in the northwest portion of Panay Island, bordering Antique to the southwest, and Capiz to the southeast.`,
+        backdrop: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Boracay_White_Beach.png",
+        feature: "Boracay",
+        media: "https://lp-cms-production.imgix.net/2023-05/shutterstock_editorial_1741828760.jpg?auto=format&fit=crop&ar=1:1&q=75&w=1200"
+    },
+    {
+        name: "AKLAN",
+        description: ` Aklan, officially the Province of Aklan, is a province in the Western Visayas region of the Philippines. Its capital is Kalibo. The province is situated in the northwest portion of Panay Island, bordering Antique to the southwest, and Capiz to the southeast.`,
+        backdrop: "https://upload.wikimedia.org/wikipedia/commons/c/cd/Boracay_White_Beach.png",
+        feature: "Boracay",
+        media: "https://lp-cms-production.imgix.net/2023-05/shutterstock_editorial_1741828760.jpg?auto=format&fit=crop&ar=1:1&q=75&w=1200"
+    },
+
+];
+
+let index = 1;
+
 let available = true;
-window.onclick = function() {
+function move(by) {
     if (!available) return;
+
+    index += by;
+    if (index < 0) index = data.length - 1;
+    else if (index >= data.length) index = 0 ;
+
     const wrapper = document.querySelector(".gallery .wrapper");
     const active = document.querySelector(".gallery .active");   
-    const cards = document.querySelectorAll(".gallery .card");
-    const move = cards[current].getBoundingClientRect().width + Number((getComputedStyle(wrapper).gap.replace(/[^\d]+/g,"")));
-    wrapper.style.transform = `translateX(-${move * (current + 1)}px)`;
-    active.style.opacity = "0"; 
-    current++;
+    const cards = document.querySelectorAll(".gallery .card");   
     
-    active.classList.remove("active");
-    cards[current].classList.add("active");
-    let activeCopy = active.cloneNode(true);
-        activeCopy.style.opacity = 1;
-        
-   
-    active.className = "card tobe-removed"; 
-    if (current % total == 0) {
-            const removes = document.querySelectorAll(".tobe-removed");
+    const activeIndex = Array.from(cards).indexOf(active);
+    const distance = cards[activeIndex].getBoundingClientRect().width + Number(getComputedStyle(wrapper).gap.replace(/[^\d]/g,""));
 
-            for (let i = 0;i < removes.length;i++) {
-                removes[i].remove();
-            }
-            
-            wrapper.style.transition = "0s";
-            wrapper.style.transform = "translateX(0)";
+    available = false;
+
+    if (by < 0) {
+        const firstElement = cards[0];
+        const elementClone = firstElement.cloneNode(true);
+        elementClone.classList.remove("active");
+        wrapper.appendChild(elementClone);
+        
+        let newActive = active.nextElementSibling;
+        active.style.opacity = 0;
+        active.classList.remove("active");
+        newActive.classList.add("active");
+
+        setTimeout(() => {
+            wrapper.style.transition = "1s";
+            wrapper.style.transform = `translateX(-${distance}px)`;
 
             setTimeout(() => {
-                wrapper.style.transition = "1s"
+                firstElement.remove();
+                wrapper.style.transition = "0s";
+                wrapper.style.transform = `translate(0)`;
+                available = true;
             },1000);
-            current = 0;
+        },100);
+
+    } else {
+        wrapper.style.transform = `translate(-${distance}px)`;
+        const lastElement = cards[cards.length - 1];
+        const elementClone = lastElement.cloneNode(true);
+        elementClone.style.opacity = 0;
+        wrapper.prepend(elementClone);
+
+        let newActive = active.previousElementSibling;
+        active.classList.remove("active");
+        newActive.classList.add("active");
+
+        setTimeout(() => {
+            wrapper.style.transition = "1s";
+            elementClone.style.opacity = 1;
+            wrapper.style.transform = `translateX(0)`;
+            lastElement.remove();
+            setTimeout(() => {
+                wrapper.style.transition = "0s";
+                available = true;
+            },1000);
+        },100);
     }
 
-    wrapper.appendChild(activeCopy);
-    available = false;
-    setTimeout(() => available = true,1000);
+    displayData();
+}
+
+function displayData() {
+    const title = document.querySelector(".texts h1");
+    const description = document.querySelector(".texts p");
+    
+    const indicators = document.querySelectorAll(".line-indicator > div");
+    const activeIndicator = document.querySelector(".line-indicator > div.active");
+    
+    swapActive(activeIndicator,indicators[data.length - (index + 1)]);
+}
+
+function swapActive(active,element) {
+    if (!element) return;
+    active.classList.remove("active");
+    element.classList.add("active");
 }
